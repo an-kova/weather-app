555c0e9255ea3619784c6e68f3b9aaffd5dc1160
Anna Kova <m3ta007@gmail.com>
2021-02-18 23:40:12 -0500

    Added and set up linters, Jest

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:jest/recommended",
+    "prettier",
+    "eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "max-len": [
+      "error", { ignoreComments: true }
+    ],
+    "no-console": "off",
+    "no-alert": "off",
+    "no-plusplus": [
+      "error", { allowForLoopAfterthoughts: true }
+    ],
+  },
+  plugins: ["jest"],
+};
