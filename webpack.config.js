a533f6a59d57d21ce9bcf10c9ba8e0bb93b80383
Anna Kova <m3ta007@gmail.com>
2021-02-21 23:59:54 -0500

    Added Babel and updated Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new Dotenv({
+      safe: true, // '.env.example' verifies the '.env' variables are all set.
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+};
